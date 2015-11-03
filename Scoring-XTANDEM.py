87f9e9f755f27d922c6e06f6527ba6ea632d93a4
Deaglan Crew <d.crew@liv.ac.uk>
2015-11-03 03:00:13 +0000

    Finished implementing XTandem style scoring through to returning E-Values.

--- Scoring-XTANDEM.py
+++ Scoring-XTANDEM.py
@@ -1,12 +1,12 @@
 import sys, getopt
-from multiprocessing import Pool
-from functools import partial
 import numpy as np
 import csv
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 import matplotlib.path as path
 import ast
+from MGFParse import MGFReader
+from scipy import stats
 
 def inputOutput (argv):
     options = {"maxMissedCleave":1}
@@ -34,62 +34,33 @@ def csvToDictRead (icsv):
     idata = csv.DictReader(ifile, dialect="excel-tab")
     odata = []
     for i in idata:
-        i["bIons"]= ast.literal_eval(i["bIons"])
-        i["yIons"]= ast.literal_eval(i["yIons"])
         odata.append(i)
     return odata
              
 #returns hyper scores for matches in data at the M2 level
-def returnHyperScore (iPeakList, norm, m1Match, tolerance):
+def returnHyperScore (iPeakList, m1Match, tolerance):
     score = 0
     bCount = 0
     yCount = 0
     for iPeak in iPeakList:
         for bIon in m1Match["bIons"]:
             if abs(iPeak[0] - float(bIon)) < tolerance:
-                m1Match["bIons"].remove(bIon)
                 score +=iPeak[1]
                 bCount +=1
         for yIon in m1Match["yIons"]:
             if abs(iPeak[0] - float(yIon)) < tolerance:
-                m1Match["yIons"].remove(yIon)
                 score +=iPeak[1]
                 yCount +=1
-    hyperScore = (score/norm)*(np.math.factorial(bCount))*(np.math.factorial(yCount))
+    hyperScore = (score)*(np.math.factorial(bCount))*(np.math.factorial(yCount))
     return hyperScore
 
 #returns sublist of peptide dataset that has correct mass
-def MatchMasses (searchIter, mass, tolerance):
+def matchMasses (searchIter, mass, tolerance):
     output = []
     for searchTerm in searchIter:
         if abs(mass - float(searchTerm["Mass"])) < tolerance:
             output.append(searchTerm)
     return output
-
-#returns Peak dictionaries for entries in MGFfile
-def MGFReader (mgfFile):
-    outDict = {"m2Peaks":[]}
-    for line in mgfFile:
-        if "TITLE" in line:
-            outDict["name"] = line.split(" ")[0][6:]
-        if "PEPMASS" in line:
-            outDict["mass"] = float(line.split(" ")[0][8:])
-        if "CHARGE" in line:
-            outDict["charge"] = int(line[7])
-        try:
-            outDict["m2Peaks"].append(list((float(line.split(" ")[0]),float(line.split(" ")[1]))))
-        except:
-            next     
-        if "END" in line:
-            outDict["trueMass"] = float((outDict["mass"]*outDict["charge"])-outDict["charge"])
-            m2norm = np.linalg.norm([i[0] for i in outDict["m2Peaks"]])
-            outDict["norm"] = m2norm
-            yield outDict
-            outDict = {"m2Peaks":[]}
-
-def Chunk (iArray, length):
-    for i in xrange(0, len(iArray), length):
-        yield iArray[i:i+length]
 
 def returnResult(matchDB, protein):
     protein["Peptides"] =[]
@@ -100,10 +71,21 @@ def returnResult(matchDB, protein):
     protein["TotalScore"] = sum([i.Score for i in protein["Peptides"]]) 
     protein["RelativeScore"] = float(protein["TotalScore"]/protein["MaxTotalScore"])
     return protein
+
+def returnEvalue (Input_array):
+    maxScore = max(Input_array)
+    n, bins = np.histogram(Input_array, len(Input_array))
+    n = [np.log((i+1)) for i in n]
+    bins = [i for i in bins]
+    while len(n) < len(bins):
+        n.append(0)
+    regressLine = stats.linregress(bins, n)
+    return np.exp((regressLine[0]*maxScore) + regressLine[1])
     
-def Make_Histogram (Input_array, Set_name):
+def make_Histogram (Input_array, Set_name):
     fig, ax = plt.subplots()
-    n, bins = np.histogram(Input_array, 100)
+    n, bins = np.histogram(Input_array, len(Input_array))
+    n = [np.log(i) for i in n]
     left = np.array(bins[:-1])
     right = np.array(bins[1:])
     bottom = np.zeros(len(left))
@@ -128,25 +110,30 @@ print "Reading peptideDB"
 peptideDB = csvToDictRead("Ref-forward.pep")
 peakListFile = open("HighestXTandemResults.list","rb")
 peakList = [i.strip("\n") for i in peakListFile.readlines()]
-print peakList
+peakList = ["Spectrum%s" % (i) for i in peakList]
 
 for peak in MGFReader(MGFFile):
-    if peak["name"] in peakList:
-        print ("[+]Searching %s" % (peak["name"]))
-        m1Matches = MatchMasses(peptideDB, peak["trueMass"], float(0.1))
-        hyperScoreList = []
-        maxScore = 0
-        maxEntry = dict()
-        for i in m1Matches:
+    print ("[+]Searching %s" % (peak["name"]))
+    m1Matches = matchMasses(peptideDB, peak["trueMass"], float(0.1))
+    hyperScoreList = []
+    maxScore = 0
+    maxEntry = dict()
+    for i in m1Matches:
+        try:
+            i["bIons"]= ast.literal_eval(i["bIons"])
+            i["yIons"]= ast.literal_eval(i["yIons"])
             proteinSet.add(i["protein"])
-            hS = returnHyperScore(peak["m2Peaks"],peak["norm"], i, 0.5)
+            hS = returnHyperScore(peak["m2Peaks"],i, 0.5)
             hyperScoreList.append(hS)
             if hS > maxScore:
                 maxScore = hS
                 maxEntry = i
-        print maxEntry
-        Make_Histogram(hyperScoreList, peak["name"])
-    else:
-        break
-   
+        except:
+            continue
+    if len(hyperScoreList) > 3:
+        try:
+            print maxEntry["peptide"]
+            print str(returnEvalue(hyperScoreList))
+        except:
+            continue
 
